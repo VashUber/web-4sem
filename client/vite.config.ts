a7e48669e6d8f913cfae4a7ac0af4dfc53d19a47
VashUber <71492611+VashUber@users.noreply.github.com>
2023-06-14 00:22:18 +0300

    feature: infrastructure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/variables.scss";
+        `
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'ui-components': fileURLToPath(new URL('./src/uiComponents.ts', import.meta.url))
+    }
+  }
 })
